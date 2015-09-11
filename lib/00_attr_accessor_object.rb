75587cca06ff3cc6732be46cdcd0d9b886531fa6
App Academy Student <student@appacademy.io>
2015-09-11 13:55:36 -0700

    sql_object

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,19 @@
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name| #handle
+      define_method(name) do
+        self.instance_variable_get("@#{name}")
+      end
+
+      set_method = "#{name}="
+
+      define_method(set_method) do |setter|
+        self.instance_variable_set("@#{name}", "#{setter}")
+      end
+
+    end
   end
+
 end
--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,22 +5,36 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    data = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+      #{table_name}
+    SQL
+    data.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |col_name|
+      define_method(col_name) do
+        self.instance_variable_get(attributes[col_name])
+      end
+      define_method("#{col_name}=") do |setter|
+        self.instance_variable_set(attributes[col_name], setter)
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    table_name = self.to_s.tableize
+    @table_name ||= table_name
   end
 
   def self.all
-    # ...
   end
 
   def self.parse_all(results)
@@ -37,6 +51,7 @@ class SQLObject
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
